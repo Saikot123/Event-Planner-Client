36631e270f62e468c996cf316aa1c7026dc046d4
Saikot123 <saikotahmed003@gmail.com>
2025-06-05 21:46:21 +0600

    Create Event Added

--- src/pages/CreateEvent.jsx
+++ src/pages/CreateEvent.jsx
@@ -1,9 +1,80 @@
-import React from 'react';
+import Heading from '../shared/Heading'
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 const CreateEvent = () => {
+    const [startDate, setStartDate] = useState(new Date());
+
+    const handleCreateEvent = e => {
+        e.preventDefault();
+        const title = e.target.title.value;
+        const location = e.target.location.value;
+        const type = e.target.type.value;
+        const image = e.target.image.value;
+        const date = e.target.date.value;
+        const description = e.target.description.value;
+        const data = {
+            title,
+            location,
+            type,
+            image,
+            date,
+            description
+        }
+        
+    }
     return (
-        <div>
-            Create Event
+        <div className='bg-base-300'>
+            <div className='w-4/5 mx-auto py-14'>
+                <Heading
+                    message={'Create Event'}
+                ></Heading>
+                <div className="flex justify-center items-center">
+                    <div className="card w-4/5 shrink-0 shadow-2xl bg-white">
+                        <div className="card-body text-secondary">
+                            <form onSubmit={handleCreateEvent}>
+                                <fieldset className="fieldset">
+                                    {/* Title */}
+                                    <label className="label text-xl font-semibold">Title</label>
+                                    <input type="text" name='title' className="input w-full" placeholder="Event Title" />
+
+                                    {/* Location */}
+                                    <label className="label text-xl font-semibold">Event Location</label>
+                                    <input type="text" name='location' className="input w-full" placeholder="Event Location" />
+
+                                    {/* Type */}
+                                    <label className="label text-xl font-semibold">Event Type</label>
+                                    <select defaultValue="Pick Event Type" name='type'
+                                        className="select w-full">
+                                        <option disabled={true}>Pick a Type</option>
+                                        <option>Cleanup</option>
+                                        <option>Plantation</option>
+                                        <option>Donation</option>
+                                    </select>
+
+                                    {/* Image */}
+                                    <label className="label text-xl font-semibold">Event Image</label>
+                                    <input type="text" name='image' className="input w-full" placeholder="Event Image" />
+
+                                    {/* Date */}
+                                    <label className="label text-xl font-semibold">Event Date</label>
+                                    <DatePicker className='input w-full' name='date' selected={startDate} onChange={(date) => setStartDate(date)} />
+
+                                    {/* Description */}
+                                    <label className="label text-xl font-semibold">Description</label>
+                                    <textarea className="textarea w-full" name='description' placeholder="Event Description"></textarea>
+
+                                    <div className='mx-auto pt-3'>
+                                        <button className="btn w-[130px] btn-secondary ">Submit</button>
+                                    </div>
+                                </fieldset>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
